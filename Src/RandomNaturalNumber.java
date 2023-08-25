9162b36bc0a515bd97202c11e20962221bb23c4c
ТфеÑNatalyaKUZYK <2598.nata.kuzyk@gmail.com>
2023-08-25 18:30:48 +0300

    Task with star sum of digits

---
+++ Src/RandomNaturalNumber.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class RandomNaturalNumber {
+    public static void main(String[] args){
+        Random random = new Random();
+        int number = random.nextInt(100011) + 11;
+        int sum = 0;
+        System.out.println("Число:" + number);
+        while(number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+         System.out.println("Сума розрядів: " + sum);
+    }
+}
